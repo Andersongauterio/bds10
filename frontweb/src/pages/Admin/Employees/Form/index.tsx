30f3c1b4825f4f852e32996bcf6bbd82f75061fc
Anderson Gauterio <anderson.camara.gauterio@gmail.com>
2022-11-02 18:09:27 -0300

    should validate email syntax

--- frontweb/src/pages/Admin/Employees/Form/index.tsx
+++ frontweb/src/pages/Admin/Employees/Form/index.tsx
@@ -83,6 +83,10 @@ const Form = () => {
                 <input
                   {...register('email', {
                     required: 'Campo obrigatório',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Email inválido',
+                    },
                   })}
                   type="text"
                   className={`form-control base-input ${errors.name ? 'is-invalid' : ''
